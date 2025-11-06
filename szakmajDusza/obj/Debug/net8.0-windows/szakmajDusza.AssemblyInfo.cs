c90008961e61df9f49c3dd287692660f112fed00
Domixd123 <takacskraldominik@gmail.com>
2025-11-06 19:11:11 +0100

    test#3

--- szakmajDusza/obj/Debug/net8.0-windows/szakmajDusza.AssemblyInfo.cs
+++ szakmajDusza/obj/Debug/net8.0-windows/szakmajDusza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("szakmajDusza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9201d2e08435e12b2b2e9e794147f86ecea02d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3f4be787c34aae08b7d89f627029b69cff10a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("szakmajDusza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("szakmajDusza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

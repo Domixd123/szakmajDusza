02144f92e18d82cb3926dca2de1411f3801cc0aa
PetalyProgram <pesek.kristof@gmail.com>
2025-11-06 19:15:17 +0100

    added zarand

--- szakmajDusza/obj/Debug/net8.0-windows/szakmajDusza.AssemblyInfo.cs
+++ szakmajDusza/obj/Debug/net8.0-windows/szakmajDusza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("szakmajDusza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3f4be787c34aae08b7d89f627029b69cff10a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5a1cc6b833c3da922e15bef35f1645131bb0ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("szakmajDusza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("szakmajDusza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

370cceba6cf1c076988d1883bcac26e7dcb9e415
Domixd123 <takacskraldominik@gmail.com>
2025-11-07 20:38:49 +0100

    starting the app requires a custom command

--- szakmajDusza/App.xaml.cs
+++ szakmajDusza/App.xaml.cs
@@ -1,14 +1,53 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace szakmajDusza
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    //copied from dusza github, we have to use this - TKD
     public partial class App : Application
     {
-    }
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			AttachConsole(-1);
+			base.OnStartup(e);
+
+			if (e.Args.Length == 0)
+			{
+				Console.WriteLine("Használat: szakmajDusza.exe [--ui | <test_dir_path>]");
+				Shutdown(1);
+				return;
+			}
+
+			if (e.Args[0] == "--ui")
+			{
+				// Játék mód
+				return;
+			}
+
+			// Teszt mód
+			try
+			{
+				RunAutomatedTest(e.Args[0]);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			finally
+			{
+				Shutdown();
+			}
+		}
+
+		private void RunAutomatedTest(string v)
+		{
+			// ...
+		}
+
+		[DllImport("Kernel32.dll")]
+		private static extern bool AttachConsole(int processId);
+	}
 
 }

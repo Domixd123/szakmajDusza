582fd6221ae519f452a52e2f6a95bb87f0ac8851
Domixd123 <takacskraldominik@gmail.com>
2025-11-08 00:47:15 +0100

    test mode mostly
    code layout
    almost all the input management

--- szakmajDusza/App.xaml.cs
+++ szakmajDusza/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.IO;
 
 namespace szakmajDusza
 {
@@ -40,10 +41,93 @@ namespace szakmajDusza
 				Shutdown();
 			}
 		}
+		public static List<Card> Cards = new List<Card>();
+		public static Dictionary<string,Card>CardsDict = new Dictionary<string,Card>();
+		public static List<Card> Leaders = new List<Card>();
+		public static Dictionary<string, Card> LeadersDict = new Dictionary<string, Card>();
 
+		public static List<Card> Jatekos = new List<Card>();
+		public static List<Card>Pakli = new List<Card>();
+
+		public static List<Kazamata>Kazamatak=new List<Kazamata>();
 		private void RunAutomatedTest(string v)
 		{
-			// ...
+			StreamReader sr=new StreamReader(v);
+			while (!sr.EndOfStream)
+			{
+				string? line = sr.ReadLine();
+				if (line == null || line == "")
+				{
+					continue;
+				}
+				string[] data = line.Split(';');
+
+				if (data[0] == "uj kartya")
+				{
+					Cards.Add(new Card(data[1], int.Parse(data[2]), int.Parse(data[3]), data[4],false));
+				}
+				else if (data[0] == "uj vezer")
+				{
+					Leaders.Add(new Card(data[1], int.Parse(data[2]), int.Parse(data[3]), data[4], true));
+				}
+				else if (data[0]=="uj kazamata")
+				{
+					if(data[1] == "egyszeru")
+					{
+						Kazamatak.Add(new Kazamata(data[2], data[1], data[4],new List<Card>() { CardsDict[data[3]] }));
+					}
+					else if(data[1] == "kis")
+					{
+						//gotta finish this
+					}
+
+
+						
+
+				}
+				else if (data[0]=="uj jatekos")
+				{
+					//i dont think we need to add anything into here
+				}
+				else if (data[0]=="felvetel gyujtemenybe")
+				{
+					if (CardsDict.ContainsKey(data[1]))
+					{
+						Jatekos.Add(CardsDict[data[1]]);
+					}
+					else if (LeadersDict.ContainsKey(data[1]))
+					{
+						Jatekos.Add(LeadersDict[data[1]]);
+					}
+				}
+				else if (data[0] =="uj pakli")
+				{
+					string[]kartyanevek=data[1].Split(',');
+					for (int i = 0; i < kartyanevek.Length; i++)
+					{
+						if (CardsDict.ContainsKey(kartyanevek[i]))
+						{
+							Pakli.Add(CardsDict[kartyanevek[i]]);
+						}
+						else if (LeadersDict.ContainsKey(kartyanevek[i]))
+						{
+							Pakli.Add(LeadersDict[kartyanevek[i]]);
+						}
+					}
+				}
+				else if(data[0] =="export vilag")
+				{
+
+				}
+				else if (data[0] == "export jatekos")
+				{
+
+				}
+				else if (data[0] == "harc")
+				{
+
+				}
+			}
 		}
 
 		[DllImport("Kernel32.dll")]
---
+++ szakmajDusza/Kazamata.cs
@@ -0,0 +1,48 @@
+﻿namespace szakmajDusza
+{
+	public class Kazamata
+	{
+		public string Name { get; set; }
+		public List<Card> Defenders { get; set; }
+		public Type Tipus { get; set; }
+		public Reward reward {  get; set; }
+		public Kazamata(string name, string type, string reward,List<Card> defenders)
+		{
+			Name = name;
+			Tipus = type switch
+			{
+				"egyszeru" => Type.egyszeru,
+				"kis" => Type.kis,
+				"nagy" => Type.nagy,
+				_ => Type.kis
+			};
+			this.reward = reward switch
+			{
+				"eletero" => Reward.eletero,
+				"sebzes" => Reward.sebzes,
+				_ => Reward.newcard
+			};
+			Defenders = defenders;
+		}
+		public bool Equals(Kazamata? obj)
+		{
+			if (obj.Name == this.Name && obj.Defenders==this.Defenders && obj.Tipus == this.Tipus&&obj.reward==this.reward)
+			{
+				return true;
+			}
+			return false;
+		}
+		public enum Type : byte
+		{
+			egyszeru,
+			kis,
+			nagy
+		}
+		public enum Reward : byte
+		{
+			eletero,
+			sebzes,
+			newcard
+		}
+	}
+}

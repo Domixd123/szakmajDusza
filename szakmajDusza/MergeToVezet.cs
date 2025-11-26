3fbe9f721212063d7cb9c2009047e68df54d88e4
IDKwhat13 <71666361+IDKwhat13@users.noreply.github.com>
2025-11-26 10:55:35 +0100

    FelaldozhatsMostmarHaromKArtyatEgyVezerert

---
+++ szakmajDusza/MergeToVezet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szakmajDusza
+{
+    public class MergeToVezet
+    {
+        public static void haromToVezer(Card elso, Card masodik, Card harmaid)
+        {
+            //MainWindow.Gyujtemeny;
+            //MainWindow.AllLeaders;
+            if (elso.Vezer||masodik.Vezer||harmaid.Vezer)
+            {
+                return;
+                //Kristóf implement error vmessage in UI
+            }
+            List<Card> obtainedVezer = new List<Card>();
+            foreach (var item in MainWindow.Gyujtemeny)
+            {
+                if (item.Vezer)
+                {
+                    obtainedVezer.Add(item);
+                }
+            }
+            if (obtainedVezer.Count>=MainWindow.AllLeaders.Count)
+            {
+                return;
+                //Kristóf implement error vmessage in UI
+            }
+            for (int i = 0; i < MainWindow.Gyujtemeny.Count; i++)
+            {
+                if (MainWindow.Gyujtemeny[i].Name==elso.Name|| MainWindow.Gyujtemeny[i].Name == masodik.Name|| MainWindow.Gyujtemeny[i].Name == harmaid.Name)
+                {
+                    MainWindow.Gyujtemeny.RemoveAt(i);
+                }
+            }
+            for (int i = 0; i < MainWindow.Jatekos.Count; i++)
+            {
+                if (MainWindow.Jatekos[i].Name == elso.Name || MainWindow.Jatekos[i].Name == masodik.Name || MainWindow.Jatekos[i].Name == harmaid.Name)
+                {
+                    MainWindow.Jatekos.RemoveAt(i);
+                }
+            }
+            //int x = 0;
+            for (int i = 0; i < MainWindow.AllLeaders.Count; i++)
+            {
+                int x = 0;
+                for (int j = 0; j < obtainedVezer.Count; j++)
+                {
+                    if (MainWindow.AllLeaders[i].Name==obtainedVezer[j].Name)
+                    {
+                        x++;
+                    }
+                }
+                if (x==0)
+                {
+                    Card ujVezer = new Card(MainWindow.AllLeaders[i].Name, MainWindow.AllLeaders[i].Damage, MainWindow.AllLeaders[i].HP, MainWindow.AllLeaders[i].Tipus.ToString(),true);
+                    MainWindow.Gyujtemeny.Add(ujVezer);
+                    break;
+                }
+            }
+            
+        }
+    }
+}

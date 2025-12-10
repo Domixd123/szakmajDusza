0f9d03cfbe23b22b3f7ce52b2c0fd47da853b958
IDKwhat13 <71666361+IDKwhat13@users.noreply.github.com>
2025-12-10 20:59:33 +0100

    DOMINIKFIXTHORONSNJKSFNJKSMD

--- szakmajDusza/MainWindow.xaml.cs
+++ szakmajDusza/MainWindow.xaml.cs
@@ -1605,7 +1605,8 @@ namespace szakmajDusza
 		}
 		private void SelectForModify(Card k)
 		{
-			VezerAlapKartya.ItemsSource = AllCardsDict.Keys;
+            SelectedCard_Wrap.Children.Clear();
+            VezerAlapKartya.ItemsSource = AllCardsDict.Keys;
             if (AllCardsDict.Count > 1||k.Vezer)
             {
                 VezerCheck.IsEnabled = true;
@@ -2468,7 +2469,7 @@ namespace szakmajDusza
 			internalEdits = true;
 			LeaderCardPanel.Visibility = Visibility.Collapsed;
 			BasicCardPanel.Visibility = Visibility.Visible;
-
+			SelectedCard_Wrap.Children.Clear();
 			VezerCheck.IsChecked = false;
 			
 			internalEdits = false;

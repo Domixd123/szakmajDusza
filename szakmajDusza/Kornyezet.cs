3fb0a66f5b41319ccef21c71af3fa378b6d2f940
IDKwhat13 <71666361+IDKwhat13@users.noreply.github.com>
2025-12-11 10:54:17 +0100

    Update Kornyezet.cs

--- szakmajDusza/Kornyezet.cs
+++ szakmajDusza/Kornyezet.cs
@@ -8,7 +8,7 @@ namespace szakmajDusza
 {
     public class Kornyezet
     {
-        //alapinformációk s
+        //alapinformációk
         public string Name { get; set; }
         public List<Card> SimaKartyak { get; set; }
         public List<Card> VezerKartyak { get; set; }

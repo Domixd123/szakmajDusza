d5c073023ca7ff20a41acf2c8011abbe337babd6
Domixd123 <takacskraldominik@gmail.com>
2025-12-11 10:16:21 +0100

    new file

---
+++ szakmajDusza/AbilityManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+using System.Data;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace szakmajDusza
+{
+	public partial class MainWindow : Window
+	{
+	}
+}
